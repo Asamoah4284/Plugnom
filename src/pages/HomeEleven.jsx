c81f13b316b09f34a5f5d87f95b4234156c9ca65
Asamoah4284 <richardasamoah284@gmail.com>
2024-11-13 15:09:45 +0000

    second commit

--- src/pages/HomeEleven.jsx
+++ src/pages/HomeEleven.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import HeaderEleven from "../components/header/HeaderEleven";
-// import HomeElevenBanner from "../components/home-eleven/HomeElevenBanner";
-// import HomeElevenDomainPricing from "../components/home-eleven/HomeElevenDomainPricing";
-// import HomeElevenPricing from "../components/home-eleven/HomeElevenPricing";
-// import HomeElevenFindDomain from "../components/home-eleven/HomeElevenFindDomain";
+
 import HomeElevenWhyChoose from "../components/home-eleven/HomeElevenWhyChoose";
 import HomeElevenTestimonials from "../components/home-eleven/HomeElevenTestimonials";
 import HomeElevenFaq from "../components/home-eleven/HomeElevenFaq";
 import HomeElevenCta from "../components/home-eleven/HomeElevenCta";
 import HomeElevenFooter from "../components/footer/HomeElevenFooter";
-// import HomeElevenDomainFinder from "../components/home-eleven/HomeElevenDomainFinder";
-// import HomeNineDomainFinder from "../components/home-nine/HomeNineDomainFinder";
 import HomeElevenDomainFinder from "../components/home-eleven/HomeElevenDomainFinder";
 
 function HomeEleven() {
@@ -18,11 +13,6 @@ function HomeEleven() {
     <div className="style__eleven">
       <HeaderEleven />
       <HomeElevenDomainFinder />
-      {/* <HomeNineDomainFinder /> */}
-      {/* <HomeElevenBanner /> */}
-      {/* <HomeElevenDomainPricing className="domain__listing"/> */}
-      {/* <HomeElevenPricing /> */}
-      {/* <HomeElevenFindDomain /> */}
       <HomeElevenWhyChoose className="whychoose__eleven" />
       <HomeElevenTestimonials />
       <HomeElevenFaq />
